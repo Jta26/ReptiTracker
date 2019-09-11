b7e29b7a884188f09128f72c0b1bf1e3f6fd469f
Jta26 <Jta26@pitt.edu>
2019-09-11 08:41:00 -0700

    Added first tests

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -2,13 +2,16 @@
  * @format
  */
 
-import React from 'react';
-import 'react-native';
-import App from '../src/App';
+import React from "react";
+import "react-native";
+import App from "../src/App";
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import renderer from "react-test-renderer";
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+it("renders correctly", () => {
+    const tree = renderer.create(
+        <App/>,
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
 });
